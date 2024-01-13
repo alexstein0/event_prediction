889d964766f245b70535b71c1085b495ef0342aa
Monte Hoover <monte.b.hoover@gmail.com>
2024-01-13 16:10:28 -0500

    Add gradient clipping, gradient accumulation, and perplexity metric.

--- event_prediction/models/trainer_utils.py
+++ event_prediction/models/trainer_utils.py
@@ -30,12 +30,13 @@ class LightingWrapper(L.LightningModule):
     the original model weights from the checkpoint dict.
     """
 
-    def __init__(self, model, loss_fn, optim, lr):
+    def __init__(self, model, loss_fn, optim, lr_scheduler, grad_accum_batches=1):
         super().__init__()
         self.model = model
         self.loss_fn = loss_fn
         self.optim = optim
-        self.lr = lr
+        self.lr_scheduler = lr_scheduler
+        self.grad_accum_batches = grad_accum_batches
 
     def forward(self, X):
         """Required LightningModule method."""
@@ -43,7 +44,12 @@ class LightingWrapper(L.LightningModule):
     
     def configure_optimizers(self):
         """Required LightningModule method."""
-        return self.optim
+        lr_sched_cfg = {
+            "scheduler": self.lr_scheduler,
+            "interval": "step",
+            "frequency": self.grad_accum_batches,
+        }
+        return {"optimizer": self.optim, "lr_scheduler": self.lr_scheduler}
 
     def training_step(self, batch, batch_idx):
         """Required LightningModule method. 
@@ -56,12 +62,14 @@ class LightingWrapper(L.LightningModule):
         """Required LightningModule method.
         Gets called from .fit() and automatically handles .eval() and calls mentioned in training_step()"""
         loss = self.get_loss(batch)
+        try:
+            perplexity = torch.exp(loss)
+        except OverflowError:
+            perplexity = float("inf")
         # Lightning automatically computes the average of this loss over all the steps in the epoch
+        # The name here becomes a key we can monitor for checkpoint saving
         self.log("val_loss", loss, prog_bar=True) 
-        # TODO: add perplexity and/or accuracy metric 
-        # preds = torch.argmax(logits, dim=1)
-        # acc = (preds == y).float().mean()
-        # self.log("val_acc", acc, prog_bar=True)
+        self.log("val_perplexity", perplexity, prog_bar=True)
         return loss
     
     def get_loss(self, batch: Tuple[torch.Tensor, torch.Tensor]) -> torch.Tensor:
@@ -80,6 +88,7 @@ class LightingWrapper(L.LightningModule):
         inputs = inputs.to(self.device)
         targets = targets.to(self.device)
         logits = self.model(inputs).logits
+        # CrossEntropyLoss was written for classical classification using batch_size x num_classes, so we flatten the token dimension into the batch dimension
         logits = rearrange(logits, 'b t v -> (b t) v')  # (b, n, v) -> (b*n, v)
         targets = rearrange(targets, 'b t -> (b t)')  # (b, n) -> (b*n)
         loss = self.loss_fn(logits, targets)
@@ -105,23 +114,36 @@ class Trainer:
         self.val_loader = val_loader
 
         if cfg.loss_fn == "CrossEntropyLoss":
-            self.loss_fn = CrossEntropyLoss()
+            loss_fn = CrossEntropyLoss()
         else:
             raise ValueError(f"Expected 'CrossEntropyLoss' but got {cfg.loss_fn}")
 
         if cfg.optim == "AdamW":
-            self.optim = AdamW(model.parameters(), lr=cfg.lr)
+            optim = AdamW(model.parameters(), lr=cfg.lr)
         else:
             raise ValueError(f"Expected 'AdamW' but got {cfg.optim}")
+        
+        try:
+            lr_scheduler = transformers.get_scheduler(
+                name=cfg.lr_scheduler,
+                optimizer=optim,
+                num_warmup_steps=cfg.warmup_steps,
+                num_training_steps=len(train_loader) * cfg.epochs,
+            )
+        except ValueError as e:
+            raise ValueError(f"Expected 'linear' or 'cosine' but got {cfg.lr_scheduler}. {e}") from e
+            
 
         ckpt_dir = os.path.join(get_original_cwd(), cfg.ckpt_path)
 
-        self.wrapped_model = LightingWrapper(model, self.loss_fn, self.optim, cfg.lr)
-        self.ckpt = callbacks.ModelCheckpoint(dirpath=ckpt_dir)
+        self.wrapped_model = LightingWrapper(model, loss_fn, optim, lr_scheduler, cfg.grad_accum_batches)
+        self.ckpt = callbacks.ModelCheckpoint(dirpath=ckpt_dir, monitor="val_perplexity", mode="min")
         self.wandb_logger = L.pytorch.loggers.WandbLogger(project=cfg.project, log_model=cfg.upload_ckpt)
         self.trainer = L.Trainer(
             max_epochs=cfg.epochs,
             val_check_interval=1 / cfg.val_checks_per_epoch,
+            accumulate_grad_batches=cfg.grad_accum_batches,
+            gradient_clip_val=cfg.gradient_clip_val,
             accelerator="auto",
             callbacks=[self.ckpt],
             logger=self.wandb_logger,
@@ -129,13 +151,6 @@ class Trainer:
 
 
     def train(self):
-        # TODO:
-        # 1. logging (see wandb from CV class project)
-        # 2. Add perplexity and/or accuracy metric (see huggingface example)
-        # 3. gradient accumulation: https://huggingface.co/learn/nlp-course/chapter7/6#training-with-accelerate
-        # 4. Look at the optimizer and hyperparameters used in the hugging face example
-        # 5. Add ability to start training from a checkpoint (CV class project)
-        # 6. [only if needed ] Use Lighting or Huggingface Accelerate for multi-gpu (see CV class project)
         self.trainer.fit(self.wrapped_model, self.train_loader, self.val_loader)
         self.wandb_logger.experiment.finish()
         statedict_path = self.save_statedict(self.ckpt.best_model_path)
